96a9311871c2945a826f83ea427005dbe783fc01
Iriahn <iriahn22@gmail.com>
2026-04-12 13:45:57 +0200

    Arreglar ocultación menú desplegable al hacer click

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -2,6 +2,9 @@
 let desplegable = document.querySelector(".hamburguer");
 // Variable para guardar contenedor del contenido del deplegable
 let contenido = document.querySelector("#hamburguer-content");
+// Variables que recogen los elementos del menú desplegable
+let opcdespuno = document.querySelectorAll(".nivel-uno");
+let opcdespdos = document.querySelectorAll(".nivel-dos");
 // Variable para almacenar la posición del scroll vertical
 let pos;
 
@@ -12,15 +15,25 @@ function mostrar(){
     pos = window.scrollY;
 }
 
-function ocultar(){
+function ocultarscroll(){
     /* Para evitar que se active por microscroll, por ejemplo al querer cerrarlo manualmente,
     lo que bloquea abrirlo, comprueba si el scroll hecho es mayor a, por ejemplo 50 */
     if(window.scrollY - pos > 50 && !contenido.classList.contains("hamburguer-nocontent"))
       contenido.classList.add("hamburguer-nocontent");
 }
 
-// Solo se 
+function ocultar(){
+    contenido.classList.add("hamburguer-nocontent");
+}
+
+// Solo se aplican las funciones si el ancho de ventana es tamaño móvil
 if(window.innerWidth < 760){
-    window.addEventListener("scroll", ocultar);
+    window.addEventListener("scroll", ocultarscroll);
     desplegable.addEventListener("click", mostrar);
+    for(let i = 0; i < opcdespuno.length; i++){
+        opcdespuno[i].addEventListener("click", ocultar);
+    }
+    for(let i = 0; i < opcdespdos.length; i++){
+        opcdespdos[i].addEventListener("click", ocultar);
+    }
 }
